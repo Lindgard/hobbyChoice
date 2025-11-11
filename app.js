c47ba817a7e494e925a0c2b36d4caf02890fd0e0
ALP <a.b.lindgard@gmail.com>
2025-11-11 13:11:33 +0100

    created randomizer functions and started creating DOM elements for future work

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const hobbiesList = document.getElementById("hobbyListPrint");
 const navbar = document.getElementById("navbar");
+const randomizerBtn = document.getElementById("randomizerBtn");
+const heroSection = document.getElementById("heroSection");
+const txtBlock = document.createElement("P");
+const newLi = document.createElement("LI");
 
 const jsonData = async () => {
   try {
@@ -8,15 +12,66 @@ const jsonData = async () => {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json();
-    console.log(data);
+    return data;
   } catch (error) {
     console.error("Error fetching data:", error);
   }
 };
 
-jsonData();
-// console.log(jsonData);
-for (let i = 0; i < jsonData.length; i++) {
-  //* create a loop that iterates new list items
-  //* for each hobby within the unordered list
-}
+const hobbyArray = await jsonData();
+
+console.log(hobbyArray);
+
+const categories = (data) => {
+  let arr = Object.keys(data);
+  let i = Math.floor(Math.random() * arr.length);
+  return arr[i];
+};
+
+console.log(categories(hobbyArray));
+
+const randomHobby = (data) => {
+  const result = hobbyArray[categories(data)];
+
+  let i = Math.floor(Math.random() * result.length);
+  return result[i];
+};
+
+console.log(randomHobby(hobbyArray));
+
+//* knapp som viser et resultat basert på randomHobby
+//* funksjonen som er skrevet over
+randomizerBtn.addEventListener("click", () => {
+  const hobby = randomHobby(hobbyArray);
+  txtBlock.classList.add("output-txt");
+  txtBlock.innerHTML = `${hobby.navn} <br> ${hobby.beskrivelse}`;
+  hobby.navn.classList.add("hobby-name");
+  hobby.beskrivelse.classList.add("hobby-description");
+  heroSection.appendChild(txtBlock);
+});
+
+//* funksjon som skal vise en liste basert
+//* på kategoriene fra JSON fil
+const createListItem = () => {
+  //? bruke array mapping?
+
+  newLi.innerText = `${hobbyArray.navn}`;
+  hobbiesList.appendChild(newLi);
+};
+
+//* console.log(data.hobbyer.[randomCategory][randomHobby]);
+
+// for (let i = 0; i < jsonData.length; i++) {
+//   //* create a loop that iterates new list items
+//   //* for each hobby within the unordered list
+// }
+
+//? Fischer-Yates Shuffle Algorithm attempt
+//* const randomCategory = (data) => {
+//*   for (let i = data.length - 1; i > 0; i--) {
+//*     let j = Math.floor(Math.random() * (i + 1));
+
+//*     [data[i], data[j]] = [data[j], data[i]];
+//*   }
+//* };
+//? console.log(randomCategory());
